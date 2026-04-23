b936da724a30a45b606dae56c955c26ec59ac13a
vsitaraman <vs19g@fsu.edu>
2026-04-23 17:21:52 -0400

    modified:   .gitignore 	modified:   ELoss/Eloss.py  made changes to generate a ELoss lookup table instead of Range and dE/dx 	new file:   ELoss/alpha_lookup_20MeV.dat 	new file:   ELoss/alpha_lookup_6.0MeV.dat 	new file:   ELoss/proton_lookup_20MeV.dat 	new file:   ELoss/proton_lookup_6.0MeV.dat

--- ELoss/Eloss.py
+++ ELoss/Eloss.py
@@ -1,77 +1,62 @@
-#VSitaraman 2024-06-20
-# This script calculates the stopping power and range for protons and alpha particles in a 96% He + 4% CO2 gas mixture at 250 Torr using the PyCatima library
 import pycatima as catima
 import numpy as np
 
-# --- 1. Experimental & Physical Constants ---
+# --- 1. Constants ---
 P_TORR = 250
 TEMP_K = 293.15 
 R = 8.3144
-MEV2U = 1.0 / 931.494  # Conversion factor for mass
+MEV2U = 1.0 / 931.494
 
-# Pressure to Pascal and Molar Density
+# Gas Density Calculations
 p_pa = P_TORR * 133.322
 molar_density = p_pa / (R * TEMP_K)
+m_he, m_c, m_o = 4.0026, 12.0000, 15.9949
+m_mix_avg = (0.96 * m_he) + (0.04 * (m_c + 2*m_o))
+rho_g_cm3 = (molar_density * m_mix_avg) / 1e6
+print(f"Gas density at {P_TORR} Torr: {rho_g_cm3:.6e} g/cm^3")
 
-# Nuclear Data (Masses in u)
-m_he = 4.002603
-m_c  = 12.00000
-m_o  = 15.99491
-m_co2 = m_c + (2 * m_o)
-
-# Effective mass of mixture (96% He, 4% CO2)
-m_mix_avg = (0.96 * m_he) + (0.04 * m_co2)
-# Density in g/cm3
-rho = (molar_density * m_mix_avg) / 1e6
-
-# --- 2. Create the Material ---
-# We treat 0.96 and 0.04 as the stoichiometry/abundance in the mixture
-material_def = [
-    (m_he, 2, 0.96), # Helium
-    (m_c,  6, 0.04), # Carbon (from CO2)
-    (m_o,  8, 0.08)  # Oxygen (2 * 0.04 from CO2)
-]
+# --- 2. Material & Step Setup ---
+material_def = [(m_he, 2, 0.96), (m_c, 6, 0.04), (m_o, 8, 0.08)]
 gas_mix = catima.Material(material_def)
-gas_mix.density(rho)
+gas_mix.density(rho_g_cm3)
 
-# --- 3. Generation Function ---
-def save_loss_file(z, mass_u, label):
-    filename = f"{label}_loss_250torr.dat"
-    # Energy range from 0.05 MeV/u to 50 MeV/u
-    energies_u = np.logspace(-1.3, 1.7, 250) 
-    
-    # Initialize Projectile: (Mass_u, Z)
+# Thickness step settings
+step_mg_cm2 = 0.001           # 1 ug/cm2 steps as per your example
+step_g_cm2 = step_mg_cm2 / 1000.0 
+max_steps = 100000             # Adjust based on how far you want to track
+
+def generate_lookup(z, mass_u, e_start_mev, label):
+    filename = f"{label}_lookup_{e_start_mev}MeV.dat"
     projectile = catima.Projectile(mass_u, z)
     
-    header = (f"{label.capitalize()} Energy Loss | 96% He + 4% CO2 @ {P_TORR} Torr\n"
-              f"Density: {rho:.6e} g/cm3\n"
-              f"E_total(MeV) \tdE/dx(MeV/cm) \tRange(cm)")
+    current_e_total = e_start_mev
+    current_thickness_g_cm2 = 0.0
     
-    output_data = []
-    for e_u in energies_u:
-        # Set kinetic energy in MeV/u
+    output = []
+    header = f"Energy(MeV) \tmg/cm2 \tcm\nStarting Energy: {e_start_mev} MeV"
+
+    for i in range(max_steps):
+        # 1. Record current state
+        dist_cm = current_thickness_g_cm2 / rho_g_cm3
+        output.append([current_e_total, current_thickness_g_cm2 * 1000.0, dist_cm])
+        
+        # 2. Calculate energy loss for the NEXT step
+        e_u = current_e_total / mass_u
+        if e_u < 0.01: # Stop at ATIMA limit
+            break
+            
         projectile.T(e_u)
+        # dedx returns MeV / (g/cm2)
+        loss_mev = catima.dedx(projectile, gas_mix) * step_g_cm2
         
-        # Calculate stopping power (MeV / (g/cm2))
-        # Multiply by density to get MeV/cm
-        stop_power = catima.dedx(projectile, gas_mix)
-        dedx_mev_cm = stop_power * rho
-        
-        # Calculate range (g/cm2) and convert to cm
-        range_g_cm2 = catima.range(projectile, gas_mix)
-        range_cm = range_g_cm2 / rho
-        
-        e_total = e_u * mass_u
-        output_data.append([e_total, dedx_mev_cm, range_cm])
-    
-    np.savetxt(filename, output_data, fmt='%.6e', delimiter='\t', header=header)
-    print(f"File created: {filename}")
+        # 3. Update values
+        current_e_total -= loss_mev
+        current_thickness_g_cm2 += step_g_cm2
 
-# --- 4. Execute ---
-# Proton: Z=1, Mass ~1.007 u
-save_loss_file(1, 1.0078, "proton")
+    np.savetxt(filename, output, fmt='%.6f', delimiter='\t', header=header)
+    print(f"Lookup table created: {filename}")
 
-# Alpha: Z=2, Mass ~4.0026 u
-save_loss_file(2, 4.0026, "alpha")
-
-print(f"\nCompleted. Calculated gas density: {rho:.6e} g/cm3")
+# --- 3. Run ---
+# Format: generate_lookup(Z, mass_u, E_start_MeV, label)
+generate_lookup(1, 1.0078, 20, "proton")
+generate_lookup(2, 4.0026, 20, "alpha")
